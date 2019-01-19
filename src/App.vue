4acfea7cab8838e2fd3ed4a4525c451ba6127944
Malik Ammar <ammar@tintash.com>
2019-01-19 14:47:13 +0500

    Update App.vue and add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <span v-if="!isLoggedIn">
+        <router-link to="/login">Login</router-link>|
+        <router-link to="/signup">Signup</router-link>
+      </span>
+      <span v-if="isLoggedIn">
+        <router-link to="/feature1">Feature1</router-link>|
+        <a @click="logout">Logout</a>
+      </span>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  computed: {
+    isLoggedIn: function() {
+      console.log(this.$store);
+      return this.$store.getters.isLoggedIn;
+    }
+  },
+  created: function() {
+    this.$http.interceptors.response.use(undefined, function(err) {
+      return new Promise(function(resolve, reject) {
+        if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+          this.$store.dispatch(this.logout);
+        }
+        throw err;
+      });
+    });
+  },
+  mounted: function() {
+    if (this.isLoggedIn) {
+      this.$router.push("/");
+    }
+  },
+  methods: {
+    logout: function() {
+      this.$store.dispatch("logout").then(() => {
+        this.$router.push("/login");
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
